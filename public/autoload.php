67ffd656c87c137cd21b0b5f154bbed44dc121ff
yaronm <yaron.miro@gmail.com>
2019-08-07 12:26:14 +0300

    clean code

--- public/autoload.php
+++ public/autoload.php
@@ -1,9 +1,7 @@
 <?php
 
-define('APPLICATION_PATH', dirname(__FILE__));
-
 spl_autoload_register(function($className) {
     // Using PSR-0 convention.
     $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-	include_once APPLICATION_PATH . DIRECTORY_SEPARATOR . $className . '.php';
+	include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . $className . '.php';
 });
